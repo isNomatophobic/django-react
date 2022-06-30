69f26038273194fb67d046bca006dc14a13f973f
Kristian Petrov <kristian@fleetpal.io>
2022-06-30 23:24:38 +0300

    Initial commit

---
+++ server/api/views.py
@@ -0,0 +1,8 @@
+from .models import Room
+from rest_framework import generics
+from .serializers import RoomSerializer
+# Create your views here.
+
+class RoomView(generics.ListAPIView):
+    queryset = Room.objects.all()
+    serializer_class = RoomSerializer
